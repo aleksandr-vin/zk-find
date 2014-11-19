d0ddbed4cafcdb42c6cb54533173c66352d3cad6
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2014-11-19 11:01:59 +0000

    Add simple implementation of the FIND utility for Zookeeper

---
+++ zk-find.py
@@ -0,0 +1,50 @@
+#
+# This is a FIND utility for Zookeeper
+#
+
+import logging
+import logging.config
+
+try:
+    logging.config.fileConfig('logging.conf')
+except:
+    logging.basicConfig()
+
+logger = logging.getLogger('zk-find')
+
+from kazoo.client     import KazooClient
+from kazoo.client     import KazooState
+from kazoo.exceptions import NoNodeError
+
+def list_children(parent):
+    try:
+        for node in zk.get_children(parent):
+            path = parent + "/" + node
+            print path
+            list_children(path)
+    except NoNodeError:
+        pass
+
+from sys import argv
+
+path = ''
+hosts = '127.0.0.1:2181'
+if len(argv) > 2:
+    hosts = argv[1]
+    path  = argv[2]
+elif len(argv) > 1:
+    path  = argv[1]
+
+def my_listener(state):
+    if state == KazooState.LOST:
+        logger.debug('Session lost')
+    elif state == KazooState.SUSPENDED:
+        logger.debug('Session suspended')
+    else:
+        logger.info('Session connected')
+
+zk = KazooClient(hosts=hosts)
+zk.add_listener(my_listener)
+zk.start()
+list_children(path)
+zk.stop()
